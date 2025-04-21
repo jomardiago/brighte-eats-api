fce0ba6f4b4daefb14ce437a68375b70f5f18ea2
jomardiago <jomardiago@gmail.com>
2025-04-21 17:53:50 +0800

    Settin up versioning, cors, helmet and validation transformer and pipes.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
+import { ValidationPipe, VersioningType } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+
+import helmet from "helmet";
 
 import { AppModule } from "./app.module";
 import { EnvConfigService } from "./env-config/env-config.service";
@@ -7,6 +10,23 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const envConfigService = app.get(EnvConfigService);
+
+  app.setGlobalPrefix("api");
+  app.enableCors();
+  app.use(helmet());
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: "v",
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    })
+  );
 
   await app.listen(envConfigService.getPort() || 5000);
 }
